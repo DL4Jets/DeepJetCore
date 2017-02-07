d6cf55589930fd015c075a8ea9aa22bd8e43cae0
Markus <markus.stoye@cern.ch>
2017-02-07 13:26:43 +0100

    cleaned up

--- Train/examples/DeepJetTrain.py
+++ Train/examples/DeepJetTrain.py
@@ -31,9 +31,9 @@ def Sequential_model(input_shape):
 # more on https://keras.io/getting-started/functional-api-guide/
 
 if len(x.shape) != 4:
-    print 'macro intended for 2D convolutions! Input must be 4D' 
+    print ('macro intended for 2D convolutions! Input must be 4D')
 input_shape = (x.shape[1],x.shape[2],x.shape[3])
-model = Sequential_model(inputshape)
+model = Sequential_model(input_shape)
 
 sgd = SGD()
 from keras.optimizers import Adam
@@ -51,7 +51,7 @@ history = History()
 #TBcallback = TensorBoard(log_dir='./logs', histogram_freq=0, write_graph=True, write_images=True)
 
 # the actual training
-model.fit(x, y ,validation_split=0.99, nb_epoch=50, batch_size=1000, callbacks=[nBatchLogger,history])
+model.fit(x, y ,validation_split=0.99, nb_epoch=5, batch_size=1000, callbacks=[history])
 
 #The below plots the loss curve for the batches, this is higher granularity than the history (per epoch)
 #plt.plot(nBatchLogger.losses)
