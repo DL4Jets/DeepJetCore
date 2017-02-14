2e63a33f68358e4d3ebff77b6d24e5f9d1ff1ada
Markus Stoye <mstoye@mstoyemac.dyndns.cern.ch>
2017-02-14 15:51:08 +0100

    more general array check

--- convertFromRoot/example/makeMeansStd.py
+++ convertFromRoot/example/makeMeansStd.py
@@ -6,11 +6,11 @@ from preprocessing import meanNormProd
 
 
 rfile = ROOT.TFile(sys.argv[1]+".root")
-tree = rfile.Get("tree")
+tree = rfile.Get("deepntuplizer/tree")
 Tuple = tree2array(tree)
-truth_check = Tuple['gen_pt']
-Njets = truth_check.shape[0]
-validTruth = truth_check > 0.
+#truth_check = Tuple['gen_pt']
+#Njets = truth_check.shape[0]
+#validTruth = truth_check > 0.
 ## No we make a files to get the means and std.
 TupleMeanStd =  meanNormProd(Tuple) 
 numpy.save(sys.argv[1]+"MeansStd.npy",TupleMeanStd)
