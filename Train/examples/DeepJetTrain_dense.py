67c6691c8c1608ee855cff1fce5df3581d11d7fd
Markus Stoye <mstoye@mstoyemac.dyndns.cern.ch>
2017-02-15 13:15:32 +0100

    intreduced mudol for models

--- Train/examples/DeepJetTrain_dense.py
+++ Train/examples/DeepJetTrain_dense.py
@@ -9,9 +9,8 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
-from keras import backend as K 
-
-
+from keras import backend as K
+from keras.layers import Input
 #zero padding done before
 #from keras.layers.convolutional import Cropping1D, ZeroPadding1D
 from keras.optimizers import SGD
@@ -19,6 +18,7 @@ from keras.optimizers import SGD
 ## to call it from cammand lines
 import sys
 import os
+
 inputDataDir = sys.argv[1]
 if inputDataDir[-1] != "/":
     inputDataDir+="/"
@@ -26,36 +26,36 @@ outputFilesTag = sys.argv[2]
 outputDir = inputDataDir+outputFilesTag+"/"
 os.mkdir(outputDir)
 import shutil
-shutil.copyfile("DeepJetTrain_regr_class_dense_conv.py",outputDir+"DeepJetTrain_regr_class_dense_conv.py")
+shutil.copyfile(sys.argv[0],outputDir+sys.argv[0])
+shutil.copyfile('DeepJet_models.py',outputDir+'DeepJet_models.py')
 
-reg_truth = np.load(inputDataDir+'regres_truth.npy')
-x_local = np.load(inputDataDir+'local_X.npy')
-x_global = np.load(inputDataDir+'global_X.npy')
-x_global = np.array( x_global.tolist() )
-class_truth = np.load(inputDataDir+'class_truth.npy')
+# here we read the data
+features = np.load(inputDataDir+'global_X.npy')
+features = np.array( features.tolist() )
+labels = np.load(inputDataDir+'class_truth.npy')
 # using view would be quicker but longer syntax
-class_truth = np.array(class_truth.tolist())
+labels = np.array(labels.tolist())
 weights = np.load(inputDataDir+'weights.npy')
-from keras.layers import Input
-inputs = [Input(shape=(6,5,122)),Input(shape=(5,))]
+inputs = Input(shape=(5,))
 
+#from from keras.models import Sequential
+from DeepJet_models import Dense_model
+model = Dense_model(inputs)
 
-from DeepJet_models import Incept_model
-model = Incept_model(inputs)
-
-sgd = SGD()
 from keras.optimizers import Adam
 adam = Adam(lr=0.005)
-model.compile(loss=['mean_squared_error','categorical_crossentropy'], optimizer=adam,loss_weights=[.005, 10.])
+model.compile(loss='categorical_crossentropy', optimizer=adam)
 
 # This stores the history of the training to e.g. allow to plot the learning curve
 from keras.callbacks import History # , TensorBoard
 # loss per epoch
 history = History()
 
+# Tensorbord file (illustrated NN structure)
+#TBcallback = TensorBoard(log_dir='./logs', histogram_freq=0, write_graph=True, write_images=True)
 
 # the actual training
-model.fit([x_local, x_global], [reg_truth,class_truth] ,validation_split=0.99, nb_epoch=20, batch_size=1000, callbacks=[history], sample_weight=[weights,weights])
+model.fit(features, labels,validation_split=0.90, nb_epoch=20, batch_size=1000, callbacks=[history], sample_weight=weights)
 
 # summarize history for loss for trainin and test sample
 plt.plot(history.history['loss'])
---
+++ Train/examples/DeepJet_models.py
@@ -0,0 +1,52 @@
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten, Convolution2D, merge, Input
+from keras.models import Model
+
+def Incept_model(inputs,dropoutRate=0.25):
+    """
+        This NN adds two inputs, one for a conv net and a seceond for a dense net, both nets get combined. The last layer is split into regression and classification activations (softmax, linear)
+    """
+    
+    x =   Convolution2D(20, 1 , 1, border_mode='same', activation='relu',init='lecun_uniform')(inputs[0])
+    # add more layers to get deeper
+    x =   Convolution2D(10, 1 , 1, border_mode='same', activation='relu',init='lecun_uniform')(x)
+    x =   Convolution2D(5, 1 , 1, border_mode='same', activation='relu',init='lecun_uniform')(x)
+    x = Flatten()(x)
+    #  Here add e.g. the normal dense stuff from DeepCSV
+    y = Dense(1, activation='relu',init='lecun_uniform',input_shape=(1,))(inputs[1])
+    y = Dense(1, activation='relu',init='lecun_uniform')(y)
+    y = Dense(1, activation='relu',init='lecun_uniform')(y)
+    # add more layers to get deeper
+    
+    # combine convolutional and dense (global) layers
+    x = merge( [x,y ] , mode='concat')
+    
+    # linear activation for regression and softmax for classification
+    x = Dense(100, activation='relu',init='lecun_uniform')(x)
+    x = Dropout(dropoutRate)(x)
+    x=  Dense(100, activation='relu',init='lecun_uniform')(x)
+    x = Dropout(dropoutRate)(x)
+    x=  Dense(100, activation='relu',init='lecun_uniform')(x)
+    #x = Dropout(dropoutRate)(x)
+    # add more layers to get deeper
+    
+    predictions = [Dense(1, activation='linear',init='normal')(x),Dense(4, activation='softmax',init='lecun_uniform')(x)]
+    model = Model(input=inputs, output=predictions)
+    return model
+
+def Dense_model(inputs,dropoutRate=0.25):
+    """
+    Dense matrix, defaults similat to 2016 training
+    """
+    #  Here add e.g. the normal dense stuff from DeepCSV
+    x = Dense(100, activation='relu',init='lecun_uniform',input_shape=(5,))(inputs)
+    x = Dense(100, activation='relu',init='lecun_uniform')(x)
+    x = Dropout(dropoutRate)(x)
+    x = Dense(100, activation='relu',init='lecun_uniform')(x)
+    x = Dropout(dropoutRate)(x)
+    x = Dense(100, activation='relu',init='lecun_uniform')(x)
+    x = Dropout(dropoutRate)(x)
+    x=  Dense(100, activation='relu',init='lecun_uniform')(x)
+    predictions = Dense(4, activation='softmax',init='lecun_uniform')(x)
+    model = Model(input=inputs, output=predictions)
+    return model
