3c80017bd61130ff3f0a9339eb020783f1b37e54
jkiesele <jkiesele@cern.ch>
2017-03-17 19:20:17 +0100

    deleted obsolete files

--- environment/env.sh
+++
@@ -1,8 +0,0 @@
-
-
-source activate deepjet
-export PYTHONPATH=`pwd`/../modules:$PYTHONPATH
-export PATH=`pwd`/../scripts:$PATH
-
-#to avoid stack overflow due to very large python arrays
-ulimit -s `ulimit -s -H`
