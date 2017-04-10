308839834ecbcb40e24b8c5fafff0da28aec8d6a
jkiesele <jkiesele@cern.ch>
2017-04-10 11:18:23 +0200

    missing files

---
+++ modules/interface/helper.h
@@ -0,0 +1,18 @@
+/*
+ * helper.h
+ *
+ *  Created on: 8 Apr 2017
+ *      Author: jkiesele
+ */
+
+#ifndef DEEPJET_MODULES_INTERFACE_HELPER_H_
+#define DEEPJET_MODULES_INTERFACE_HELPER_H_
+
+
+#include <dirent.h>
+#include <stdlib.h>
+#include "TString.h"
+
+TString prependXRootD(const TString& path);
+
+#endif /* DEEPJET_MODULES_INTERFACE_HELPER_H_ */
---
+++ modules/src/helper.cpp
@@ -0,0 +1,23 @@
+/*
+ * helper.cpp
+ *
+ *  Created on: 8 Apr 2017
+ *      Author: jkiesele
+ */
+
+
+#include "../interface/helper.h"
+
+
+TString prependXRootD(const TString& path){
+
+    TString full_path = realpath(path, NULL);
+    if(full_path.BeginsWith("/eos/cms/")){
+        TString append="root://eoscms.cern.ch//";
+        TString s_remove="/eos/cms/";
+        TString newpath (full_path(s_remove.Length(),full_path.Length()));
+        newpath=append+newpath;
+        return newpath;
+    }
+    return path;
+}
