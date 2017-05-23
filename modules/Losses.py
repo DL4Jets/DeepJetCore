3401c24e0dc70d0cecd2c4bac5c26ad69b1d9e6c
Markus <markus.stoye@cern.ch>
2017-05-23 15:59:52 +0200

    added some references

--- modules/Losses.py
+++ modules/Losses.py
@@ -1,6 +1,12 @@
 from keras import backend as K
 
 def loss_NLL(y_true, x):
+    """
+    This loss is the negative log likelyhood for gaussian pdf.
+    See e.g. http://bayesiandeeplearning.org/papers/BDL_29.pdf for details
+    Generally it might be better to even use Mixture density networks (i.e. more complex pdf than single gauss, see:
+    https://publications.aston.ac.uk/373/1/NCRG_94_004.pdf
+    """
     x_pred = x[:,1:]
     x_sig = x[:,:1]
     return K.mean(0.5* K.log(K.square(x_sig))  + K.square(x_pred - y_true)/K.square(x_sig)/2.,    axis=-1)
