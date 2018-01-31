e42bdb67ca589f1e7d43b3d4bbc451a83abff638
jkiesele <jkiesele@cern.ch>
2018-01-31 11:44:50 +0100

    small fixes

--- environment/gpu_env.sh
+++ environment/gpu_env.sh
@@ -6,5 +6,3 @@ export PYTHONPATH=`pwd`/../modules:$PYTHONPATH
 export LD_LIBRARY_PATH=`pwd`/../modules:$LD_LIBRARY_PATH
 export PATH=`pwd`/../scripts:$PATH
 export LD_PRELOAD=$CONDA_PREFIX/lib/libmkl_core.so:$CONDA_PREFIX/lib/libmkl_sequential.so
-
-export DEEPJET=`pwd`/../
