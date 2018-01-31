c2e6b3136ced6b3eb35cf1bc2a01495b9868e6f9
jkiesele <jkiesele@cern.ch>
2018-01-31 17:23:28 +0100

    not working restructuring commit

---
+++ gpu_env.sh
@@ -0,0 +1,8 @@
+
+
+source activate deepjetLinux3_gpu
+
+export PYTHONPATH=`pwd`/../:$PYTHONPATH
+export LD_LIBRARY_PATH=`pwd`/compiled:$LD_LIBRARY_PATH
+export PATH=`pwd`/bin:$PATH
+export LD_PRELOAD=$CONDA_PREFIX/lib/libmkl_core.so:$CONDA_PREFIX/lib/libmkl_sequential.so
--- lxplus_env.sh
+++ lxplus_env.sh
@@ -1,9 +1,10 @@
 
 
 source activate deepjetLinux3
-export PYTHONPATH=`pwd`/../modules:$PYTHONPATH
-export LD_LIBRARY_PATH=`pwd`/../modules:$LD_LIBRARY_PATH
-export PATH=`pwd`/../scripts:$PATH
+
+export PYTHONPATH=`pwd`/../:$PYTHONPATH
+export LD_LIBRARY_PATH=`pwd`/compiled:$LD_LIBRARY_PATH
+export PATH=`pwd`/bin:$PATH
 export LD_PRELOAD=$CONDA_PREFIX/lib/libmkl_core.so:$CONDA_PREFIX/lib/libmkl_sequential.so
 
 
