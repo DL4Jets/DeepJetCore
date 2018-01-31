9b0bb986cf92367a0975031f06a50b1238c14f3d
jkiesele <jkiesele@cern.ch>
2018-01-31 11:40:10 +0100

    first restructuring - not working

--- modules/TrainData.py
+++ modules/TrainData.py
@@ -106,34 +106,16 @@ class TrainData(object):
         
         '''
         
-        self.treename="deepntuplizer/tree"
-        
-        self.undefTruth=['isUndefined']
-        
-        self.referenceclass='isB'
-        
-        self.truthclasses=['isB','isBB','isGBB','isLeptonicB','isLeptonicB_C','isC','isCC',
-                           'isGCC','isUD','isS','isG','isUndefined']
-        
+        self.treename=""
+        self.undefTruth=[]  
+        self.referenceclass=''
+        self.truthclasses=[]
         self.allbranchestoberead=[]
         
-        #standard branches
-        self.registerBranches(self.undefTruth)
-        self.registerBranches(self.truthclasses)
-        self.registerBranches(['jet_pt','jet_eta'])
-        
-        self.weightbranchX='jet_pt'
-        self.weightbranchY='jet_eta'
-        
-        self.weight_binX = numpy.array([
-                10,25,30,35,40,45,50,60,75,100,
-                125,150,175,200,250,300,400,500,
-                600,2000],dtype=float)
-        
-        self.weight_binY = numpy.array(
-            [-2.5,-2.,-1.5,-1.,-0.5,0.5,1,1.5,2.,2.5],
-            dtype=float
-            )
+        self.weightbranchX=''
+        self.weightbranchY=''
+        self.weight_binX = numpy.array([-1e12, 1e12],dtype=float)
+        self.weight_binY = numpy.array([-1e12, 1e12],dtype=float)
         
         self.reducedtruthclasses=[]
         self.regressiontargetclasses=[]
@@ -224,14 +206,6 @@ class TrainData(object):
         else:
             return self.truthclasses
     
-    def addFromRootFile(self,fileName):
-        '''
-        Adds from a root file and randomly shuffles the input
-        '''
-        raise Exception('to be implemented')
-        #just call read from root (virtual in python??), and mix with existing x,y,weight
-
-
 
     def reduceTruth(self, tuple_in=None):
         self.reducedtruthclasses=self.truthclasses
@@ -700,63 +674,7 @@ class TrainData(object):
         return weighter
     
         
-    def getFlavourClassificationData(self,filename,TupleMeanStd, weighter):
-        from stopwatch import stopwatch
-        
-        sw=stopwatch()
-        swall=stopwatch()
-        
-        import ROOT
-        
-        fileTimeOut(filename,120) #give eos a minute to recover
-        rfile = ROOT.TFile(filename)
-        tree = rfile.Get(self.treename)
-        self.nsamples=tree.GetEntries()
-        
-        #print('took ', sw.getAndReset(), ' seconds for getting tree entries')
     
-        
-        Tuple = self.readTreeFromRootToTuple(filename)
-        
-        
-        x_all = MeanNormZeroPad(filename,TupleMeanStd,self.branches,self.branchcutoffs,self.nsamples)
-        
-        #print('took ', sw.getAndReset(), ' seconds for mean norm and zero padding (C module)')
-        
-        notremoves=numpy.array([])
-        weights=numpy.array([])
-        if self.remove:
-            notremoves=weighter.createNotRemoveIndices(Tuple)
-            weights=notremoves
-            #print('took ', sw.getAndReset(), ' to create remove indices')
-        elif self.weight:
-            #print('creating weights')
-            weights= weighter.getJetWeights(Tuple)
-        else:
-            print('neither remove nor weight')
-            weights=numpy.empty(self.nsamples)
-            weights.fill(1.)
-        
-        
-        
-        truthtuple =  Tuple[self.truthclasses]
-        #print(self.truthclasses)
-        alltruth=self.reduceTruth(truthtuple)
-        
-        #print(alltruth.shape)
-        if self.remove:
-            #print('remove')
-            weights=weights[notremoves > 0]
-            x_all=x_all[notremoves > 0]
-            alltruth=alltruth[notremoves > 0]
-       
-        newnsamp=x_all.shape[0]
-        #print('reduced content to ', int(float(newnsamp)/float(self.nsamples)*100),'%')
-        self.nsamples = newnsamp
-        
-        #print('took in total ', swall.getAndReset(),' seconds for conversion')
-        
-        return weights,x_all,alltruth, notremoves
 
     def _normalize_input_(self, weighter, npy_array):
         weights = None
@@ -765,8 +683,9 @@ class TrainData(object):
             self.w = [weights for _ in self.y]
         elif self.remove:
             notremoves=weighter.createNotRemoveIndices(npy_array)
-            undef=npy_array['isUndefined']
-            notremoves-=undef
+            if self.undefTruth:
+                undef=npy_array[self.undefTruth].sum(axis=1)
+                notremoves-=undef
             print(' to created remove indices')
             weights=notremoves
 
@@ -786,180 +705,3 @@ class TrainData(object):
 
         
 
-from preprocessing import MeanNormApply, MeanNormZeroPad
-
-class TrainData_Flavour(TrainData):
-    '''
-    
-    '''
-    def __init__(self):
-        TrainData.__init__(self)
-        self.clear()
-        
-    
-    def readFromRootFile(self,filename,TupleMeanStd, weighter):
-        
-        weights,x_all,alltruth, _ =self.getFlavourClassificationData(filename,TupleMeanStd, weighter)
-        
-        self.w=[weights]
-        self.x=[x_all]
-        self.y=[alltruth]
-        
-     
-     
-class TrainData_simpleTruth(TrainData):
-    def __init__(self):
-        TrainData.__init__(self)
-        self.clear()
-        
-    def reduceTruth(self, tuple_in):
-        
-        self.reducedtruthclasses=['isB','isBB','isC','isUDSG']
-        if tuple_in is not None:
-            b = tuple_in['isB'].view(numpy.ndarray)
-            bl = tuple_in['isLeptonicB'].view(numpy.ndarray)
-            blc = tuple_in['isLeptonicB_C'].view(numpy.ndarray)
-            allb = b+bl+blc
-
-            bb = tuple_in['isBB'].view(numpy.ndarray)
-            gbb = tuple_in['isGBB'].view(numpy.ndarray)            
-           
-            c = tuple_in['isC'].view(numpy.ndarray)
-            cc = tuple_in['isCC'].view(numpy.ndarray)
-            gcc = tuple_in['isGCC'].view(numpy.ndarray)
-           
-            ud = tuple_in['isUD'].view(numpy.ndarray)
-            s = tuple_in['isS'].view(numpy.ndarray)
-            uds=ud+s
-            g = tuple_in['isG'].view(numpy.ndarray)
-            l = g + uds
-
-            return numpy.vstack((allb,bb+gbb,c+cc+gcc,l)).transpose()
-    
-    
-class TrainData_leptTruth(TrainData):
-    def __init__(self):
-        TrainData.__init__(self)
-        self.clear()
-        
-    def reduceTruth(self, tuple_in):
-        
-        self.reducedtruthclasses=['isB','isBB','isLeptB','isC','isUDSG']
-        if tuple_in is not None:
-            b = tuple_in['isB'].view(numpy.ndarray)
-            bb = tuple_in['isBB'].view(numpy.ndarray)
-            gbb = tuple_in['isGBB'].view(numpy.ndarray)
-            
-            
-            bl = tuple_in['isLeptonicB'].view(numpy.ndarray)
-            blc = tuple_in['isLeptonicB_C'].view(numpy.ndarray)
-            lepb=bl+blc
-           
-            c = tuple_in['isC'].view(numpy.ndarray)
-            cc = tuple_in['isCC'].view(numpy.ndarray)
-            gcc = tuple_in['isGCC'].view(numpy.ndarray)
-           
-            ud = tuple_in['isUD'].view(numpy.ndarray)
-            s = tuple_in['isS'].view(numpy.ndarray)
-            uds=ud+s
-            
-            g = tuple_in['isG'].view(numpy.ndarray)
-            l = g + uds
-            
-            return numpy.vstack((b,bb+gbb,lepb,c+cc+gcc,l)).transpose()  
-        
-        
-        
-
-class TrainData_fullTruth(TrainData):
-    def __init__(self):
-        TrainData.__init__(self)
-        self.clear()
-        
-    def reduceTruth(self, tuple_in):
-        
-        self.reducedtruthclasses=['isB','isBB','isLeptB','isC','isUDS','isG']
-        if tuple_in is not None:
-            b = tuple_in['isB'].view(numpy.ndarray)
-            
-            bb = tuple_in['isBB'].view(numpy.ndarray)
-            gbb = tuple_in['isGBB'].view(numpy.ndarray)
-            
-            
-            bl = tuple_in['isLeptonicB'].view(numpy.ndarray)
-            blc = tuple_in['isLeptonicB_C'].view(numpy.ndarray)
-            lepb=bl+blc
-           
-            c = tuple_in['isC'].view(numpy.ndarray)
-            cc = tuple_in['isCC'].view(numpy.ndarray)
-            gcc = tuple_in['isGCC'].view(numpy.ndarray)
-           
-            ud = tuple_in['isUD'].view(numpy.ndarray)
-            s = tuple_in['isS'].view(numpy.ndarray)
-            uds=ud+s
-            
-            g = tuple_in['isG'].view(numpy.ndarray)
-            
-            
-            return numpy.vstack((b,bb+gbb,lepb,c+cc+gcc,uds,g)).transpose()    
-  
-
-class TrainData_QGOnly(TrainData):
-    def __init__(self):
-        TrainData.__init__(self)
-        self.clear()
-        self.undefTruth=['isUndefined']
-        
-        self.referenceclass='isUD'
-        
-        
-        
-    def reduceTruth(self, tuple_in):
-        
-        self.reducedtruthclasses=['isUDS','isG']
-        if tuple_in is not None:
-            #b = tuple_in['isB'].view(numpy.ndarray)
-            #bb = tuple_in['isBB'].view(numpy.ndarray)
-            #gbb = tuple_in['isGBB'].view(numpy.ndarray)
-            #
-            #
-            #bl = tuple_in['isLeptonicB'].view(numpy.ndarray)
-            #blc = tuple_in['isLeptonicB_C'].view(numpy.ndarray)
-            #lepb=bl+blc
-            #
-            #c = tuple_in['isC'].view(numpy.ndarray)
-            #cc = tuple_in['isCC'].view(numpy.ndarray)
-            #gcc = tuple_in['isGCC'].view(numpy.ndarray)
-           
-            ud = tuple_in['isUD'].view(numpy.ndarray)
-            s = tuple_in['isS'].view(numpy.ndarray)
-            uds=ud+s
-            
-            g = tuple_in['isG'].view(numpy.ndarray)
-            
-            
-            return numpy.vstack((uds,g)).transpose()    
-
-class TrainData_quarkGluon(TrainData):
-    def __init__(self):
-        super(TrainData_quarkGluon, self).__init__()
-        self.referenceclass = 'isG'
-        self.reducedtruthclasses=['isQ', 'isG']
-        self.clear()
-        
-    def reduceTruth(self, tuple_in):
-        if tuple_in is not None:
-            b = tuple_in['isB'].view(numpy.ndarray)
-            #bb = tuple_in['isBB'].view(numpy.ndarray) #this should be gluon?
-            
-            bl = tuple_in['isLeptonicB'].view(numpy.ndarray)
-            blc = tuple_in['isLeptonicB_C'].view(numpy.ndarray)
-            c = tuple_in['isC'].view(numpy.ndarray)
-            ud = tuple_in['isUD'].view(numpy.ndarray)
-            s = tuple_in['isS'].view(numpy.ndarray)
-            q = ud+s#+c+blc+bl+b
-            
-            g = tuple_in['isG'].view(numpy.ndarray)
-            return numpy.vstack((q, g)).transpose()    
-        else:
-            print('I got an empty tuple?')
---
+++ scripts/convertFromRoot.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+convertFromRoot -- converts the root files produced with the deepJet ntupler to the data format used by keras for the DNN training
+
+convertFromRoot is a small program that converts the root files produced with the deepJet ntupler to the data format used by keras for the DNN training
+
+
+@author:     jkiesele
+
+'''
+
+import sys
+import os
+
+from argparse import ArgumentParser
+from pdb import set_trace
+import logging
+logging.getLogger().setLevel(logging.INFO)
+
+__all__ = []
+__version__ = 0.1
+__date__ = '2017-02-22'
+__updated__ = '2017-02-22'
+
+DEBUG = 0
+TESTRUN = 0
+PROFILE = 0
+
+def main(argv=None):
+    '''Command line options.'''
+
+    program_name = os.path.basename(sys.argv[0])
+    program_version = "v0.1"
+    program_build_date = "%s" % __updated__
+
+    program_version_string = '%%prog %s (%s)' % (program_version, program_build_date)
+    program_longdesc = ''' ''' # optional - give further explanation about what the program does
+    program_license = "Copyright 2017 user_name (organization_name) Licensed under the Apache License 2.0\nhttp://www.apache.org/licenses/LICENSE-2.0"
+
+    import imp
+    try:
+        imp.find_module('datastructures')
+        from datastructures import *
+    except ImportError:
+        print('datastructure modules not found. Please define a DeepJetCore submodule')
+        
+    
+    class_options = structure_list
+    
+    class_options = dict((str(i).split("'")[1].split('.')[-1], i) for i in class_options)
+
+    parser = ArgumentParser('program to convert root tuples to traindata format')
+    parser.add_argument("-i", help="set input sample description (output from the check.py script)", metavar="FILE")
+    parser.add_argument("-o",  help="set output path", metavar="PATH")
+    parser.add_argument("-c",  choices = class_options.keys(), help="set output class (options: %s)" % ', '.join(class_options.keys()), metavar="Class")
+    parser.add_argument("-r",  help="set path to snapshot that got interrupted", metavar="FILE", default='')
+    parser.add_argument("-n", default='', help="(optional) number of child processes")
+    parser.add_argument("--testdatafor", default='')
+    parser.add_argument("--usemeansfrom", default='')
+    parser.add_argument("--nothreads", action='store_true')
+    parser.add_argument("--means", action='store_true', help='compute only means')
+    parser.add_argument("--batch", help='Provide a batch ID to be used')
+    parser.add_argument("-v", action='store_true', help='verbose')
+    parser.add_argument("-q", action='store_true', help='quiet')
+    
+    # process options
+    args=parser.parse_args()
+    infile=args.i
+    outPath=args.o
+    class_name=args.c    
+    recover=args.r
+    testdatafor=args.testdatafor
+    usemeansfrom=args.usemeansfrom
+    nchilds=args.n
+
+    if args.batch and not (args.usemeansfrom or args.testdatafor):
+        raise ValueError(
+            'When running in batch mode you should also '
+            'provide a means source through the --usemeansfrom option'
+            )
+
+    if args.v:
+        logging.getLogger().setLevel(logging.DEBUG)
+    elif args.q:
+        logging.getLogger().setLevel(logging.WARNING)
+
+    if infile:
+        logging.info("infile = %s" % infile)
+    if outPath:
+        logging.info("outPath = %s" % outPath)
+
+    # MAIN BODY #
+    from DataCollection import DataCollection
+    dc = DataCollection(nprocs = (1 if args.nothreads else -1))  
+    if len(nchilds):
+        dc.nprocs=int(nchilds)  
+    
+    if class_name in class_options:
+        traind = class_options[class_name]
+    elif not recover and not testdatafor:
+        raise Exception('wrong class selecton') #should never really happen as we catch it in the parser        
+    if testdatafor:
+        logging.info('converting test data, no weights applied')
+        dc.createTestDataForDataCollection(
+            testdatafor, infile, outPath, 
+            outname = args.batch if args.batch else 'dataCollection.dc',
+            batch_mode = bool(args.batch)
+        )    
+    elif recover:
+        dc.recoverCreateDataFromRootFromSnapshot(recover)        
+    elif args.means:
+        dc.convertListOfRootFiles(
+            infile, traind(), outPath, 
+            means_only=True, output_name='batch_template.dc'
+            )
+    else:
+        dc.convertListOfRootFiles(
+            infile, traind(), outPath, 
+            usemeansfrom, output_name = args.batch if args.batch else 'dataCollection.dc',
+            batch_mode = bool(args.batch)
+            )
+    
+
+
+#if __name__ == "__main__":
+if DEBUG:
+    sys.argv.append("-h")
+if TESTRUN:
+    import doctest
+    doctest.testmod()
+if PROFILE:
+    import cProfile
+    import pstats
+    profile_filename = 'convertFromRoot_profile.txt'
+    cProfile.run('main()', profile_filename)
+    statsfile = open("profile_stats.txt", "wb")
+    p = pstats.Stats(profile_filename, stream=statsfile)
+    stats = p.strip_dirs().sort_stats('cumulative')
+    stats.print_stats()
+    statsfile.close()
+    sys.exit(0)
+sys.exit(main())
