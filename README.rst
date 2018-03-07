af623a1ffc3efb2f3832a10d2c1abaa94d19d124
Swapneel Mehta <swapneel.mehta96@gmail.com>
2018-03-07 14:28:19 +0100

    restructuring directory

---
+++ README.rst
@@ -0,0 +1,6 @@
+DeepJetCore
+-----------
+
+Set of scripts used within the Experimental Physics Group in the CMS Experiment at CERN. Install as a pip package in a virtual environment such as one provided by Anaconda. Refer to the README at https://github.com/DL4Jets/DeepJetCore for installation and usage instructions::
+        
+        >>>pip install DeepJetCore
---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
+setup(name='DeepJetCore',
+      version='0.0.1',
+      description='The DeepJetCore Library: Deep Learning \
+      for High-energy Physics',
+      url='https://github.com/SwapneelM/DeepJetCore',
+      author='CMS Group (EP-CMG-PS)',
+      author_email='swapneel.mehta@cern.ch',
+      license='MIT',
+      packages=['DeepJetCore'],
+      install_requires=[
+          'cycler==0.10.0',
+          'funcsigs==1.0.2',
+          'functools32==3.2.3.post2',
+          'h5py==2.6.0',
+          'tensorflow==1.0.1',
+          'Keras==2.0.0',
+          'matplotlib==2.0.0',
+          'mock==2.0.0',
+          'pbr==2.0.0',
+          'protobuf==3.2.0',
+          'pyparsing==2.2.0',
+          'python-dateutil==2.6.0',
+          'pytz==2016.10',
+          'PyYAML==3.12',
+          'subprocess32==3.2.7'
+      ],
+      zip_safe=False)
