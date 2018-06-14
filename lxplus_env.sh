8b5d94d99bf9bc92c8d4d0223e4e49b5e44e97e2
SwapneelM <swapneel.mehta96@gmail.com>
2018-06-14 18:09:12 +0200

    Adding path to modules in DeepJet

--- lxplus_env.sh
+++ lxplus_env.sh
@@ -4,7 +4,7 @@
 
 # export DEEPJETCORE=`pwd`
 
-export PYTHONPATH=`pwd`/../:$PYTHONPATH
+export PYTHONPATH=`pwd`:$PYTHONPATH
 export LD_LIBRARY_PATH=`pwd`/compiled:$LD_LIBRARY_PATH
 export PATH=`pwd`/bin:$PATH
 export LD_PRELOAD=$CONDA_PREFIX/lib/libmkl_core.so:$CONDA_PREFIX/lib/libmkl_sequential.so
