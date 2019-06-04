d091f5143f97e2ff10a9282fb5c77ac5ee4c292b
CoralieNeubueser <jkiesele@cern.ch>
2019-06-04 10:32:07 +0200

    fix for non GPU machines

--- bin/locate_cuda.py
+++ bin/locate_cuda.py
@@ -6,10 +6,18 @@ import subprocess
 
 def locate_cuda():
     ldlibpath = str(os.environ['LD_LIBRARY_PATH'])
-    result = os.path.dirname(subprocess.check_output(['locate', 'libcublas.so.9.0']).split('\n')[0])
+    result=''
+    try:
+        result = os.path.dirname(subprocess.check_output(['locate', 'libcublas.so.9.0']).split('\n')[0])
+    except:
+        pass
     if not (result in ldlibpath):
         ldlibpath+=':'+result+'/'
-    result = os.path.dirname(subprocess.check_output(['locate', 'libcudnn.so.7']).split('\n')[0])
+    result=''
+    try:
+        result = os.path.dirname(subprocess.check_output(['locate', 'libcudnn.so.7']).split('\n')[0])
+    except: 
+        pass
     if not (result in ldlibpath):
         ldlibpath+=':'+result+'/'
     print(ldlibpath)
