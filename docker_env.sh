5432d6cb967da0f6cd51ea4b2033ccf87374063a
Jan Kieseler <jkiesele@cern.ch>
2019-10-04 16:53:43 +0200

    docker env

---
+++ docker_env.sh
@@ -0,0 +1,9 @@
+
+export PATH=`pwd`/bin:$PATH
+export PYTHONPATH=`pwd`/../:$PYTHONPATH
+if [ $LD_LIBRARY_PATH ]
+then
+    export LD_LIBRARY_PATH=`pwd`/compiled/:$LD_LIBRARY_PATH
+else
+    export LD_LIBRARY_PATH=`pwd`/compiled/
+fi
