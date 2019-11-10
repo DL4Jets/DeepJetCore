36c5034006488db911aa91eb8488dd1d2ac5b31f
Jan Kieseler <jkiesele@cern.ch>
2019-11-10 17:46:52 +0100

    better description in subpackage generator

--- bin/createSubpackage.py
+++ bin/createSubpackage.py
@@ -134,12 +134,13 @@ class TrainData_example(TrainData):
 
 
 training_template='''
+
 from DeepJetCore.training.training_base import training_base
 import keras
 from keras.models import Model
 from keras.layers import Dense, Conv2D, Flatten, BatchNormalization #etc
 
-def my_model(Inputs,nclasses,nregressions,otheroption):
+def my_model(Inputs,otheroption):
     
     x = Inputs[0] #this is the self.x list from the TrainData data structure
     x = BatchNormalization(momentum=0.9)(x)
@@ -152,7 +153,8 @@ def my_model(Inputs,nclasses,nregressions,otheroption):
     x = Flatten()(x)
     x = Dense(32, activation='relu')(x)
     
-    x = Dense(nclasses, activation='softmax')(x)
+    # 3 prediction classes
+    x = Dense(3, activation='softmax')(x)
     
     predictions = [x]
     return Model(inputs=Inputs, outputs=predictions)
@@ -174,11 +176,8 @@ model,history = train.trainModel(nepochs=10,
                                  batchsize=500,
                                  checkperiod=1, # saves a checkpoint model every N epochs
                                  verbose=1)
-
-
-
-
-
+                                 
+print('Since the training is done, use the predict.py script to predict the model output on you test sample, e.g.:\n predict.py <training output>/KERAS_model.h5 <training output>/trainsamples.djcdc <path to data>/test.txt <output dir>')
 '''
         
 datastructures_init = '''
