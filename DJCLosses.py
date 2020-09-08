5f1055971b8fca595beb502c89683e5d730c3bb2
Jan Kieseler <jkiesele@cern.ch>
2020-09-08 20:03:12 +0200

    mostly new containers

--- DJCLosses.py
+++ DJCLosses.py
@@ -6,6 +6,6 @@ import tensorflow as tf
 def dummy_loss(truth, pred):
     #t = tf.Print(truth,[truth],'truth ')
     #p = tf.Print(pred,[pred],'pred ')
-    return tf.reduce_mean(truth)+tf.reduce_mean(pred)
+    return (tf.reduce_mean(truth)-tf.reduce_mean(pred))**2
 
 djc_global_loss_list['dummy_loss']=dummy_loss
