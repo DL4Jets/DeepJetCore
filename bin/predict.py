cf5bdf6f925e9b57b4d62371d722e39d6fa4d6b4
Andrzej Novak <andrzej.novak@cern.ch>
2020-12-08 23:34:02 +0100

    chore: clean up PR

--- bin/predict.py
+++ bin/predict.py
@@ -56,8 +56,7 @@ DJCSetGPUs(args.gpu)
 
 custom_objs = get_custom_objects()
 
-print("X",args.inputModel, custom_objs )
-model=load_model(args.inputModel, custom_objects=custom_objs, compile=True)
+model=load_model(args.inputModel, custom_objects=custom_objs)
 dc = None
 if args.inputSourceFileList[-6:] == ".djcdc" and not args.trainingDataCollection[-6:] == ".djcdc":
     dc = DataCollection(args.inputSourceFileList)
