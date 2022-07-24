b9e6998acacc4aa67c9223298014e2f3790ca6e0
Jan Kieseler <jkiesele@cern.ch>
2022-07-24 11:25:10 +0200

    test auto builds

--- docker/build.sh
+++ docker/build.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/bash
 
-docker build -t cernml4reco/deepjetcore3:latest . --build-arg BUILD_DATE="$(date)"
+BASE_IMAGE_TAG=cu11.6
+
+docker build -t cernml4reco/deepjetcore3:latest . \
+       --build-arg BUILD_DATE="$(date)"  --build-arg BASE_IMAGE_TAG=$BASE_IMAGE_TAG
--- docker/build_if_needed.sh
+++ docker/build_if_needed.sh
@@ -1,9 +1,22 @@
-#!/bin/zsh
+#!/bin/bash
+
+BASE_IMAGE_TAG=cu11.6
 
 git fetch
-if [ $(git rev-parse HEAD) == $(git rev-parse @{u}) ]
+if [ $(git rev-parse HEAD) != $(git rev-parse @{u}) ]
 then
-echo no pull
-else
-echo pull
+  
+  OLD_BASE_ID=$(git rev-parse HEAD:docker/Dockerfile_base) 
+  git pull
+  NEW_BASE_ID=$(git rev-parse HEAD:docker/Dockerfile_base) 
+  
+  if [ $OLD_BASE_ID != $NEW_BASE_ID ]
+  then
+    echo "base image changed from ${OLD_BASE_ID} to ${NEW_BASE_ID}, rerunning base build"
+    echo docker build -t cernml4reco/djcbase:$BASE_IMAGE_TAG -f Dockerfile_base .
+    echo docker push --max-concurrent-uploads 3 cernml4reco/djcbase:$BASE_IMAGE_TAG
+  fi
+  
+  echo docker build -t cernml4reco/deepjetcore3:latest . \
+       --build-arg BUILD_DATE="$(date)" --build-arg BASE_IMAGE_TAG=$BASE_IMAGE_TAG
 fi
