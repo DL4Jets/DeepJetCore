d4c8af926fe78fe6bd13d7776b5cc3d6ee60d687
Jan Kieseler <jkiesele@cern.ch>
2022-07-24 12:00:23 +0200

    removing dry run

--- docker/build_if_needed.sh
+++ docker/build_if_needed.sh
@@ -22,9 +22,9 @@ then
   
   if [ $OLD_BASE_ID != $NEW_BASE_ID ] || [ $FORCE ]
   then
-    "base image changed from ${OLD_BASE_ID} to ${NEW_BASE_ID}, rerunning base build"
-    echo docker build --no-cache=true -t cernml4reco/djcbase:$BASE_IMAGE_TAG -f Dockerfile_base . > base_build.log
-    echo docker push --max-concurrent-uploads 3 cernml4reco/djcbase:$BASE_IMAGE_TAG  > base_push.log
+    echo "base image changed from ${OLD_BASE_ID} to ${NEW_BASE_ID}, rerunning base build"
+    docker build --no-cache=true -t cernml4reco/djcbase:$BASE_IMAGE_TAG -f Dockerfile_base . > base_build.log
+    docker push --max-concurrent-uploads 3 cernml4reco/djcbase:$BASE_IMAGE_TAG  > base_push.log
     
     { echo "Subject: base build finished" ; 
       cat base_build.log ; 
@@ -33,10 +33,10 @@ then
     
   fi
   
-  echo docker build --no-cache=true -t cernml4reco/deepjetcore3:latest . \
+  docker build --no-cache=true -t cernml4reco/deepjetcore3:latest . \
        --build-arg BUILD_DATE="$(date)" --build-arg BASE_IMAGE_TAG=$BASE_IMAGE_TAG \
        --build-arg COMMIT=$COMMIT  > build.log
-  echo docker push --max-concurrent-uploads 3 cernml4reco/deepjetcore3:latest > push.log
+  docker push --max-concurrent-uploads 3 cernml4reco/deepjetcore3:latest > push.log
   
   { echo "Subject: DJC build finished" ; 
     cat build.log ; 
