b04940d612dd56de6c05155118250e64ab30590a
Jan Kieseler <jkiesele@cern.ch>
2023-12-29 09:20:26 +0100

    first cleanup

--- testing/unit/TestCFunctions.py
+++
@@ -1,9 +0,0 @@
-from DeepJetCore.compiled.c_testFunctions import *
-
-import unittest
-
-
-class TestCFunctions(unittest.TestCase):
-    def test_trainDataFiller(self):
-        testTrainDataFileStreamer()
-    
--- testing/unit/test.py
+++
@@ -1,8 +0,0 @@
-from TestSimpleArray import TestSimpleArray
-from TestTrainData import TestTrainData
-from TestCompatibility import TestCompatibility
-from TestTrainDataGenerator import TestTrainDataGenerator
-from TestCFunctions import TestCFunctions
-import unittest
-
-unittest.main()
